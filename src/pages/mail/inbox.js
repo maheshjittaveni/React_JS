572a5abee09ef721111f3c70b15094577f426199
maheshjittaveni <maheshjittaveni46@gmail.com>
2021-12-13 09:11:54 +0530

    updated api calls

--- src/pages/mail/inbox.js
+++ src/pages/mail/inbox.js
@@ -1,49 +1,60 @@
 import React, {Component} from "react";
 import axios from "axios";
 
-class InboxPage extends Component{
-  loadUsers(){
-var api_url="https://reqres.in/api/users?page=2";
-axios.get(api_url)
-      .then((response)=>{
-//console.log(response)
-var serverData=response.data;
-this.setState({
-  userList=serverData.data
-})
 
-      })
-      .catch((error)=>{
-console.log(console.error());
-alert("Server is down, please try after sometimes.....")
-      });
+class InboxPage extends Component{
+
+  constructor(props){
+    super(props);
+    this.state = {
+      userList : []
+    }
+  }
+
+  loadUsers(){
+    var api_url = "https://reqres.in/api/users?page=2";
+
+    axios.get(api_url)
+        .then((response) => {
+            var serverData = response.data;
+            this.setState({
+              userList : serverData.data
+            })
+        })
+        .catch((error) => {
+            console.log(error);
+            alert("Server is down, pls try after sometimes..")
+        }); 
 
   }
+
   render(){
-    var users=this.state.userList.map((value,index)=>{
+
+    var users = this.state.userList.map((value, index) => {
       return(
         <tr key={index}>
           <td>{value.first_name}</td>
           <td>{value.last_name}</td>
           <td>{value.email}</td>
           <td>
-          <img src={value.avatar} className="profile-image"/>
+            <img src={value.avatar} className="profile-image"/>
           </td>
         </tr>
       )
     })
+
     return(
       <div>
-        <h1>This is a Index Page</h1>
-        <button onClick={()=>this.loadUsers()} >Load users from API</button>
+        <h1>This is a Inbox Page</h1>
+        <button onClick={() => this.loadUsers()}>Load Users from API</button>
 
         <table id="customers">
           <thead>
             <tr>
-              <th>First name</th>
-              <th>Last name</th>
+              <th>First Name</th>
+              <th>Last Name</th>
               <th>Email</th>
-              <th>Profile pic</th>              
+              <th>Profile Pic</th>
             </tr>
           </thead>
           <tbody>
