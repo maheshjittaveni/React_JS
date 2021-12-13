0e5540f460af226c21d47c8b7448bc0ae9e5fd83
maheshjittaveni <maheshjittaveni46@gmail.com>
2021-12-13 09:05:09 +0530

    api calls

--- src/pages/mail/inbox.js
+++ src/pages/mail/inbox.js
@@ -1,10 +1,55 @@
 import React, {Component} from "react";
+import axios from "axios";
 
 class InboxPage extends Component{
+  loadUsers(){
+var api_url="https://reqres.in/api/users?page=2";
+axios.get(api_url)
+      .then((response)=>{
+//console.log(response)
+var serverData=response.data;
+this.setState({
+  userList=serverData.data
+})
+
+      })
+      .catch((error)=>{
+console.log(console.error());
+alert("Server is down, please try after sometimes.....")
+      });
+
+  }
   render(){
+    var users=this.state.userList.map((value,index)=>{
+      return(
+        <tr key={index}>
+          <td>{value.first_name}</td>
+          <td>{value.last_name}</td>
+          <td>{value.email}</td>
+          <td>
+          <img src={value.avatar} className="profile-image"/>
+          </td>
+        </tr>
+      )
+    })
     return(
       <div>
         <h1>This is a Index Page</h1>
+        <button onClick={()=>this.loadUsers()} >Load users from API</button>
+
+        <table id="customers">
+          <thead>
+            <tr>
+              <th>First name</th>
+              <th>Last name</th>
+              <th>Email</th>
+              <th>Profile pic</th>              
+            </tr>
+          </thead>
+          <tbody>
+            {users}
+          </tbody>
+        </table>
       </div>
     )
   }
--- src/pages/mail/trash.js
+++ src/pages/mail/trash.js
@@ -1,10 +1,28 @@
 import React, {Component} from "react";
+import axios from "axios";
 
 class TrashPage extends Component{
+
+  onDelete(){
+    var api_url = "https://reqres.in/api/users/2";
+
+    axios.delete(api_url)
+        .then((response) => {
+          var serverData = response.data;
+          console.log(serverData);
+          alert("Successfully Deleted the user")
+        })
+        .catch((error) => {
+          console.log(error)
+          alert("Server down, pls try again later")
+        })
+  }
+  
   render(){
     return(
       <div>
         <h1>This is a Trash Page</h1>
+        <button onClick={() => this.onDelete()}>Delete API</button>
       </div>
     )
   }
