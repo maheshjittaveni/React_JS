6c5b18a43b6499c909c9ee46c7b6bcc09bf28d75
maheshjittaveni <maheshjittaveni46@gmail.com>
2021-12-15 10:29:10 +0530

    updated node-api

--- server.js
+++ server.js
@@ -1,49 +1,54 @@
-const express=require("express");
-const bodyParse=require("body-parser");
-const mysql=require("mysql");
-const cors=require("cors");
-const app=express();
-const http=require("http").createServer(app);
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const mysql = require("mysql");
+const app = express();
+const http = require("http").createServer(app);
 
 app.use(express.json());
-app.use(bodyParse.json());
+app.use(bodyParser.json());
 app.use(cors({
-credentials: true,
-origin: "*"
+  credentials : true,
+  origin : "*"
 }));
-var connection=mysql.createConnection({
-    host:"localhost",
-    user:"root",
-    password:"",
-    database:"vshesh"
 
+var connection = mysql.createConnection({
+  host : "localhost",
+  user : "root",
+  password : "",
+  database : "codestart2"
 });
-connection.connect((error)=>{
-if(error){
+
+connection.connect((error) => {
+  if(error){
     throw error;
-}
-console.log("My sql database connected");
+  }
+
+  console.log("MYSQL database is connected")
 });
 
-app.get("/api/user/list", (request,response)=>{
-var query= `SELECT * FROM employee_record`;
-connection.query(query, (error, result)=>{
-    if(error){
+app.get("/api/user/list", (request, response) =>{
+  var query = `SELECT * FROM employee_record`;
+
+  connection.query(query, (error, result) => {
+      if(error){
         response.status(500).send(error);
         return;
-    }
-    response.status(200).send(result);
-});
-});
-app.get("/api/user/create", (request,response)=>{
-    var firstname="request.body.first_name";
-    var lastname="request.body.last_name";
-    var emailid="request.body.email_id";
-    var age="request.body.age";
-    var location="request.body.location";
+      }
 
-    var query=`INSERT INTO employee_record(first_name, last_name, email_id, location, age) VALUES('${firstname}','${lastname}','${emailid}','${location}','${age}')`;
-    
+      response.status(200).send(result);
+  });
+});
+
+app.post("/api/user/create", (request, response) => {
+  var firstName = request.body.first_name;
+  var lastName = request.body.last_name;
+  var emailId = request.body.email_id;
+  var age = request.body.age;
+  var location = request.body.location;
+
+  var query = `INSERT INTO employee_record (first_name, last_name, eamil_id, location, age) VALUES ('${firstName}', '${lastName}', '${emailId}', '${location}', ${age})`;
+
   connection.query(query, (error, result) => {
     if(error){
       response.status(500).send(error);
@@ -58,7 +63,50 @@ app.get("/api/user/create", (request,response)=>{
 
 });
 
-const port=process.env.PORT || 4000;
-http.listen(port, ()=>{
-    console.log("Node JS server is running on port 4000")
+app.put("/api/user/edit/:id", (request, response) => {
+  var id = request.params.id;
+
+  var firstName = request.body.first_name;
+  var lastName = request.body.last_name;
+  var emailId = request.body.email_id;
+  var age = request.body.age;
+  var location = request.body.location;
+
+  var query = `UPDATE employee_record SET first_name='${firstName}', last_name='${lastName}', eamil_id='${emailId}', location='${location}', age=${age} WHERE id=${id}`;
+  
+  connection.query(query, (error, result) => {
+    if(error){
+      response.status(500).send(error);
+      return;
+    }
+
+    response.status(200).send({
+      result,
+      message : "Update the given user information"
+    })
+  })
+});
+
+app.delete("/api/user/delete/:id", (request, response) => {
+  var id = request.params.id;
+
+  var query = `DELETE FROM employee_record WHERE id=${id}`;
+
+  connection.query(query, (error, result) => {
+    if(error){
+      response.status(500).send(error);
+      return;
+    }
+    
+    response.status(200).send({
+      result,
+      message : "Successfully deleted the user profile"
+    })
+  });
 })
+
+
+const port = process.env.PORT || 4000;
+http.listen(port, () => {
+  console.log("Node JS Server is running on port 4000");
+});
