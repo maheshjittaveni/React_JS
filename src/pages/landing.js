839bcff8ae4f186e86a690242baa27f8c2c59c49
maheshjittaveni <maheshjittaveni46@gmail.com>
2021-12-15 10:28:09 +0530

    updted landing page

--- src/pages/landing.js
+++ src/pages/landing.js
@@ -1,10 +1,32 @@
 import React, {Component} from "react";
+import axios from "axios";
 
 class LandingPage extends Component{
+
+  constructor(props){
+    super(props);
+    this.state = {
+      userList : []
+    }
+  }
+
+  listUserProfile(){
+    var url = "http://localhost:4000/api/user/list";
+
+    axios.get(url)
+        .then((response) => {
+          console.log(response)
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+  }
+
   render(){
     return(
       <div>
         <h1>This is Landing Page</h1>
+        <button onClick={() => this.listUserProfile()}>Load User Information</button>
       </div>
     )
   }
